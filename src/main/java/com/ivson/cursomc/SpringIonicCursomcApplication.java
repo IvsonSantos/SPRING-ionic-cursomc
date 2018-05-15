6354d515acf122b2fa5180ea4ba0771a169e4e7f
Ivson Santos <santosivson@hotmail.com>
2018-05-14 21:48:29 -0300

    Projeto criado

---
+++ src/main/java/com/ivson/cursomc/SpringIonicCursomcApplication.java
@@ -0,0 +1,12 @@
+package com.ivson.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicCursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIonicCursomcApplication.class, args);
+	}
+}
