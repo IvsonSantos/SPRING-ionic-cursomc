74641233d29e5df53f069eb548598b2724119091
Ivson Santos <santosivson@hotmail.com>
2018-05-15 21:51:48 -0300

    Criacao da entidade Categoria

--- src/main/java/com/ivson/cursomc/domain/Categoria.java
+++ src/main/java/com/ivson/cursomc/domain/Categoria.java
@@ -2,15 +2,23 @@ package com.ivson.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Serializable, os objetos podem ser convertidos em uma sequencia de Bytes
  * @author Santo
  *
  */
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
