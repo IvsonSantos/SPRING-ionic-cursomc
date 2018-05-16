e2cf3874f5797e99423d3d3ed82ccc6837bf2d04
Ivson Santos <santosivson@hotmail.com>
2018-05-15 21:36:31 -0300

    Categoria

--- src/main/java/com/ivson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ivson/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.ivson.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ivson.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Tudo ok";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.addAll(Arrays.asList(cat1, cat2));
+		
+		return lista;
 	}
 }
