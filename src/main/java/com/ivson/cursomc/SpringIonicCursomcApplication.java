788ce708f8ddc72b7383cf6f56382971ce7fbc5b
Ivson Santos <santosivson@hotmail.com>
2018-05-16 19:12:23 -0300

    Inserindo uma categoria no CommandLineRunner

--- src/main/java/com/ivson/cursomc/SpringIonicCursomcApplication.java
+++ src/main/java/com/ivson/cursomc/SpringIonicCursomcApplication.java
@@ -1,12 +1,32 @@
 package com.ivson.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringIonicCursomcApplication {
+import com.ivson.cursomc.domain.Categoria;
+import com.ivson.cursomc.repository.CategoriaRepository;
+import com.ivson.cursomc.service.CategoriaService;
 
+@SpringBootApplication
+public class SpringIonicCursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicCursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
